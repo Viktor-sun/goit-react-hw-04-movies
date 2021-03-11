387dc36e5a0f055b5e18075b800bd0b4737802b3
Viktor-sun <bobick.zdoh@gmail.com>
2021-03-11 23:25:02 +0200

    add queries

--- src/views/MoviesView.js
+++ src/views/MoviesView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
 import * as api from '../service/api-movies';
 import TitleOnError from '../components/TitleOnError';
 import SearchForm from '../components/SearchForm';
@@ -10,14 +11,41 @@ class MoviesView extends Component {
     error: null,
   };
 
+  componentDidMount() {
+    const query = this.parseQueryFromProps(this.props).query;
+
+    if (query) {
+      api
+        .fetchSearchMovies(query)
+        .then(data => {
+          this.setState({ searchMoviesArr: data.results });
+        })
+        .catch(error => this.setState({ error }));
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevQuery = this.parseQueryFromProps(prevProps).query;
+    const nextQuery = this.parseQueryFromProps(this.props).query;
+
+    if (prevQuery !== nextQuery) {
+      api
+        .fetchSearchMovies(nextQuery)
+        .then(data => {
+          this.setState({ searchMoviesArr: data.results });
+        })
+        .catch(error => this.setState({ error }));
+    }
+  }
+
   formSubmit = searchQuery => {
-    api
-      .fetchSearchMovies(searchQuery)
-      .then(data => {
-        this.setState({ searchMoviesArr: data.results });
-      })
-      .catch(error => this.setState({ error }));
+    this.props.history.push({
+      pathname: this.props.location.pathname,
+      search: `query=${searchQuery}`,
+    });
   };
+
+  parseQueryFromProps = props => queryString.parse(props.location.search);
 
   render() {
     const { searchMoviesArr, error } = this.state;
