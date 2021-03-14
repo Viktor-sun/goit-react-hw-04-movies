8b1b72b6026882b11d5a461918a31e1db6300297
Viktor-sun <bobick.zdoh@gmail.com>
2021-03-14 21:13:01 +0200

    add search page

--- src/service/api-movies.js
+++ src/service/api-movies.js
@@ -4,11 +4,11 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.headers.common['Authorization'] =
   'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZWU4YTM1NmE4MjE3ZDgyM2Q4MTRlZGFhY2ViNjJmYiIsInN1YiI6IjYwMTAwMTY3YjJlMDc0MDAzZjBhNGY5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a1RnNSBZJWsTzFEhbSbIMfsNxSRWL6flGymQDpaEpFc';
 
-const fetchPopularMovies = () =>
-  axios.get('/trending/all/day').then(res => res.data);
+const fetchPopularMovies = page =>
+  axios.get(`/trending/all/day?page=${page}`).then(res => res.data);
 
-const fetchSearchMovies = query =>
-  axios.get(`/search/movie?query=${query}`).then(res => res.data);
+const fetchSearchMovies = (query, page) =>
+  axios.get(`/search/movie?query=${query}&page=${page}`).then(res => res.data);
 
 const fetchInfoByMovies = movieId =>
   axios.get(`/movie/${movieId}`).then(res => res.data);
