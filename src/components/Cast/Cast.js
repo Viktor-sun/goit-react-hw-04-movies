5d2ce28cf55d078365e57e86d53e7330705d4125
Viktor-sun <bobick.zdoh@gmail.com>
2021-08-09 22:59:56 +0300

    add styles

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -11,8 +11,6 @@ const Cast = ({ dataCast }) => {
         {dataCast.length > 0 &&
           dataCast.map(({ id, name, character, profile_path }) => (
             <li key={id}>
-              <h3>{name}</h3>
-              <p>{character}</p>
               <img
                 src={`https://image.tmdb.org/t/p/w500${
                   !profile_path
@@ -21,6 +19,8 @@ const Cast = ({ dataCast }) => {
                 }`}
                 alt={name}
               />
+              <h3>{name}</h3>
+              <p>{character}</p>
             </li>
           ))}
       </ul>
